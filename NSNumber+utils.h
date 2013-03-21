6438de623f6ec5c9b235580eeb5834a8de6644e7
Evgeny Yagrushkin <yagrushkin@gmail.com>
2013-03-21 00:31:20 -0400

    added currency formatter for NSNumber

---
+++ NSNumber+utils.h
@@ -0,0 +1,15 @@
+//
+//  NSNumber+utils.h
+//  Cravit
+//
+//  Created by Evgeny Yagrushkin on 2013-03-21.
+//  Copyright (c) 2013 Evgeny Yagrushkin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (utils)
+
+- (NSString*) currencyString;
+
+@end
---
+++ NSNumber+utils.m
@@ -0,0 +1,21 @@
+//
+//  NSNumber+utils.m
+//  Cravit
+//
+//  Created by Evgeny Yagrushkin on 2013-03-21.
+//  Copyright (c) 2013 Evgeny Yagrushkin. All rights reserved.
+//
+
+#import "NSNumber+utils.h"
+
+@implementation NSNumber (utils)
+
+- (NSString*) currencyString{
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    [numberFormatter setFormatterBehavior:NSNumberFormatterBehavior10_4];
+    [numberFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    [numberFormatter setLocale:[NSLocale currentLocale]];
+    return [numberFormatter stringFromNumber:self];
+}
+
+@end
