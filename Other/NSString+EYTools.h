20a5f3ff079e156c248b0081081e2bec74a664e6
eugene <yagrushkin@gmail.com>
2013-07-02 14:35:57 -0400

    new string helper

---
+++ Other/NSString+EYTools.h
@@ -0,0 +1,16 @@
+//
+//  NSString+EYTools.h
+//  UIToolsDemo
+//
+//  Created by Evgeny Yagrushkin on 2013-07-02.
+//  Copyright (c) 2013 Evgeny Yagrushkin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (EYTools)
+
+- (NSString *)Base64EncodedStringFromString;
++ (NSString *)GetUUID;
+
+@end
---
+++ Other/NSString+EYTools.m
@@ -0,0 +1,50 @@
+//
+//  NSString+EYTools.m
+//  UIToolsDemo
+//
+//  Created by Evgeny Yagrushkin on 2013-07-02.
+//  Copyright (c) 2013 Evgeny Yagrushkin. All rights reserved.
+//
+
+#import "NSString+EYTools.h"
+
+@implementation NSString (EYTools)
+
+- (NSString *)Base64EncodedStringFromString{
+    NSData *data = [NSData dataWithBytes:[self UTF8String] length:[self lengthOfBytesUsingEncoding:NSUTF8StringEncoding]];
+    NSUInteger length = [data length];
+    NSMutableData *mutableData = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
+    
+    uint8_t *input = (uint8_t *)[data bytes];
+    uint8_t *output = (uint8_t *)[mutableData mutableBytes];
+    
+    for (NSUInteger i = 0; i < length; i += 3) {
+        NSUInteger value = 0;
+        for (NSUInteger j = i; j < (i + 3); j++) {
+            value <<= 8;
+            if (j < length) {
+                value |= (0xFF & input[j]);
+            }
+        }
+        
+        static uint8_t const kAFBase64EncodingTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+        
+        NSUInteger idx = (i / 3) * 4;
+        output[idx + 0] = kAFBase64EncodingTable[(value >> 18) & 0x3F];
+        output[idx + 1] = kAFBase64EncodingTable[(value >> 12) & 0x3F];
+        output[idx + 2] = (i + 1) < length ? kAFBase64EncodingTable[(value >> 6)  & 0x3F] : '=';
+        output[idx + 3] = (i + 2) < length ? kAFBase64EncodingTable[(value >> 0)  & 0x3F] : '=';
+    }
+    
+    return [[NSString alloc] initWithData:mutableData encoding:NSASCIIStringEncoding];
+}
+
++ (NSString *)GetUUID
+{
+    CFUUIDRef theUUID = CFUUIDCreate(NULL);
+    CFStringRef string = CFUUIDCreateString(NULL, theUUID);
+    CFRelease(theUUID);
+    return (NSString *)CFBridgingRelease(string);
+}
+
+@end
