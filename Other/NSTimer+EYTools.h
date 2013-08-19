ba2497da36d6c931a15d1ad6653ae2efc1607121
eugene <yagrushkin@gmail.com>
2013-08-19 11:06:55 -0400

    files for NSTimer category

---
+++ Other/NSTimer+EYTools.h
@@ -0,0 +1,13 @@
+//
+//  NSTimer+EYTools.h
+//  BreakfastTelevision
+//
+//  Created by Evgeny Yagrushkin on 2013-08-19.
+//  Copyright (c) 2013 Rogers Media. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSTimer (EYTools)
+
+@end
---
+++ Other/NSTimer+EYTools.m
@@ -0,0 +1,13 @@
+//
+//  NSTimer+EYTools.m
+//  BreakfastTelevision
+//
+//  Created by Evgeny Yagrushkin on 2013-08-19.
+//  Copyright (c) 2013 Rogers Media. All rights reserved.
+//
+
+#import "NSTimer+EYTools.h"
+
+@implementation NSTimer (EYTools)
+
+@end
