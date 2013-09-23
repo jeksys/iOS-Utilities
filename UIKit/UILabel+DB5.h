513cbb2b4c70e738aa22372aaa9f98c67fe23a87
Eugene Yagrushkin <yagrushkin@gmail.com>
2013-09-23 13:01:42 -0400

    added DB5 helpers

---
+++ UIKit/UILabel+DB5.h
@@ -0,0 +1,16 @@
+//
+//  UILabel+DB5.h
+//  BreakfastTelevision
+//
+//  Created by Evgeny Yagrushkin on 2013-09-04.
+//  Copyright (c) 2013 Rogers Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (DB5)
+
+- (void) DB5WithTheme:(VSTheme*)theme andKey:(NSString*)key;
+
+
+@end
---
+++ UIKit/UILabel+DB5.m
@@ -0,0 +1,21 @@
+//
+//  UILabel+DB5.m
+//  BreakfastTelevision
+//
+//  Created by Evgeny Yagrushkin on 2013-09-04.
+//  Copyright (c) 2013 Rogers Media. All rights reserved.
+//
+
+#import "UILabel+DB5.h"
+#import <VSTheme.h>
+
+@implementation UILabel (DB5)
+
+- (void) DB5WithTheme:(VSTheme*)theme andKey:(NSString*)key{
+
+    self.font = [theme fontForKey:[key stringByAppendingString:@"Font"]];
+    self.textColor = [theme colorForKey:[key stringByAppendingString:@"FontColor"]];
+
+}
+
+@end
