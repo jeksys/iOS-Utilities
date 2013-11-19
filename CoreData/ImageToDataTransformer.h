7dc6408f27b8a1f2b75347a74382546b612edccf
eugene <yagrushkin@gmail.com>
2013-11-19 12:01:00 -0500

    added ImageToData transformer for CoreData

---
+++ CoreData/ImageToDataTransformer.h
@@ -0,0 +1,13 @@
+//
+//  ImageToDataTransformer.h
+//  Today's Parents
+//
+//  Created by Evgeny Yagrushkin on 11/19/2013.
+//  Copyright (c) 2013 Rogers Media. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageToDataTransformer : NSValueTransformer
+
+@end
---
+++ CoreData/ImageToDataTransformer.m
@@ -0,0 +1,13 @@
+//
+//  ImageToDataTransformer.m
+//  Today's Parents
+//
+//  Created by Evgeny Yagrushkin on 11/19/2013.
+//  Copyright (c) 2013 Rogers Media. All rights reserved.
+//
+
+#import "ImageToDataTransformer.h"
+
+@implementation ImageToDataTransformer
+
+@end
