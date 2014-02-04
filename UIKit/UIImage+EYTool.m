e9b766d593bb8999358ff9ea6438b63f86e435d4
eugene <yagrushkin@gmail.com>
2014-02-04 12:56:51 -0500

    added sample code to blur a view

--- UIKit/UIImage+EYTool.m
+++ UIKit/UIImage+EYTool.m
@@ -402,4 +402,27 @@
     return result;
 }
 
+//-(UIImage *)blurredSnapshot
+//{
+//    // Create the image context
+//    UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, self.window.screen.scale);
+//    
+//    // There he is! The new API method
+//    [self drawViewHierarchyInRect:self.frame afterScreenUpdates:NO];
+//    
+//    // Get the snapshot
+//    UIImage *snapshotImage = UIGraphicsGetImageFromCurrentImageContext();
+//    
+//    // Now apply the blur effect using Apple's UIImageEffect category
+//    UIImage *blurredSnapshotImage = [snapshotImage applyLightEffect];
+//    
+//    // Or apply any other effects available in "UIImage+ImageEffects.h"
+//    // UIImage *blurredSnapshotImage = [snapshotImage applyDarkEffect];
+//    // UIImage *blurredSnapshotImage = [snapshotImage applyExtraLightEffect];
+//    
+//    // Be nice and clean your mess up
+//    UIGraphicsEndImageContext();
+//    
+//    return blurredSnapshotImage;
+//}
 @end
